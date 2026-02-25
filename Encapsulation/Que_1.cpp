43bf64208d458fc8ed624b6a3423970aaa874951
yug patel <pyug31196@gmail.com>
2026-02-25 12:05:33 +0530

    Add files via upload

---
+++ Encapsulation/Que_1.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+using namespace std;
+
+class Student{
+	
+	private:
+		
+		int id,age;
+		string name,course,city,email,collage;
+	
+	public:
+		
+		void Stu_id(int id){
+			this->id = id;
+		}
+		
+		void Stu_age(int age){
+			this->age = age;
+		}
+		
+		void Stu_name(string name){
+			this->name = name
+
+};
+
+int main(){
+	
+	Student s1,s2,s3,s4,s5;
+	
+	
+	
+	return 0;
+}
